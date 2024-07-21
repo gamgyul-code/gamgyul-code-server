29ee35b746023dc98532068d9f75bb58c00ea569
king_0417 <73704053+gywns0417@users.noreply.github.com>
2024-07-21 13:58:46 +0900

    chore : init project

---
+++ src/main/java/com/gamgyul_code/halmang_vision/HalmangVisionApplication.java
@@ -0,0 +1,13 @@
+package com.gamgyul_code.halmang_vision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalmangVisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HalmangVisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamgyul_code/halmang_vision/HalmangVisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamgyul_code.halmang_vision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalmangVisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
