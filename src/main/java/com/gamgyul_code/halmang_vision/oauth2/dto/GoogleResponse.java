96c826b7825f49013cb1578aaacd5fd4c6c11b63
gywns0417 <gywns0417@gmail.com>
2024-07-31 21:42:04 +0900

    feat: 소셜 로그인 업체별 응답 구현

---
+++ src/main/java/com/gamgyul_code/halmang_vision/oauth2/dto/GoogleResponse.java
@@ -0,0 +1,27 @@
+package com.gamgyul_code.halmang_vision.oauth2.dto;
+
+import java.util.Map;
+
+public class GoogleResponse implements Oauth2Response{
+
+    private final Map<String, Object> attribute;
+
+    public GoogleResponse(Map<String, Object> attribute) {
+        this.attribute = attribute;
+    }
+
+    @Override
+    public String getProvider() {
+        return "google";
+    }
+
+    @Override
+    public String getProviderId() {
+        return attribute.get("sub").toString();
+    }
+
+    @Override
+    public String getEmail() {
+        return attribute.get("email").toString();
+    }
+}
---
+++ src/main/java/com/gamgyul_code/halmang_vision/oauth2/dto/KakaoResponse.java
@@ -0,0 +1,36 @@
+package com.gamgyul_code.halmang_vision.oauth2.dto;
+
+import java.util.Map;
+
+public class KakaoResponse implements Oauth2Response {
+
+    private final Map<String, Object> attributes;
+    private final Map<String, Object> kakaoAccount;
+    private final Map<String, Object> kakaoProfile;
+
+    public KakaoResponse(Map<String, Object> attributes) {
+        this.attributes = attributes;
+        this.kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        this.kakaoProfile = (Map<String, Object>) kakaoAccount.get("profile");
+    }
+
+    @Override
+    public String getProvider() {
+        return "kakao";
+    }
+
+    @Override
+    public String getProviderId() {
+        return attributes.get("id").toString();
+    }
+
+    @Override
+    public String getEmail() {
+        return kakaoAccount.get("email").toString();
+    }
+
+    public String getProfileImageUrl() {
+        Object profileImageUrl = kakaoProfile.get("profile_image_url");
+        return profileImageUrl != null ? profileImageUrl.toString() : "No profile image URL provided";
+    }
+}
