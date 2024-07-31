aa010a70e3fcd1b685cc05aea03abd8329ae8ed4
gywns0417 <gywns0417@gmail.com>
2024-07-31 21:41:33 +0900

    feat: 회원 관련 코드 작성

---
+++ src/main/java/com/gamgyul_code/halmang_vision/member/application/MemberService.java
@@ -0,0 +1,38 @@
+package com.gamgyul_code.halmang_vision.member.application;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+
+    public RedirectView logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+
+        clearCookies(request, response);
+        return new RedirectView("/"); // 로그아웃 후 리디렉션할 URL
+    }
+
+    private void clearCookies(HttpServletRequest request, HttpServletResponse response) {
+        for (Cookie cookie : request.getCookies()) {
+            cookie.setValue("");
+            cookie.setPath("/");
+            cookie.setMaxAge(0);
+            response.addCookie(cookie);
+        }
+    }
+}
---
+++ src/main/java/com/gamgyul_code/halmang_vision/member/domain/Member.java
@@ -0,0 +1,30 @@
+package com.gamgyul_code.halmang_vision.member.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NotBlank
+    @Size(max = 30)
+    @Email(regexp = "^[a-zA-Z0-9._%+-]{1,20}@[a-zA-Z0-9.-]{1,10}\\.[a-zA-Z]{2,}$")
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    private String username;
+}
