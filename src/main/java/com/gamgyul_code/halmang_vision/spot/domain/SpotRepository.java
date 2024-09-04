554412a7239ef5fff031656de0b0f6c680aad112
king_0417 <73704053+gywns0417@users.noreply.github.com>
2024-09-05 01:43:12 +0900

    feat: 예외 처리를 위한 JPA 메서드 작성

--- src/main/java/com/gamgyul_code/halmang_vision/spot/domain/SpotRepository.java
+++ src/main/java/com/gamgyul_code/halmang_vision/spot/domain/SpotRepository.java
@@ -1,6 +1,9 @@
 package com.gamgyul_code.halmang_vision.spot.domain;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SpotRepository extends JpaRepository<Spot, Long> {
+
+    Optional<Spot> findByName(String name);
 }
