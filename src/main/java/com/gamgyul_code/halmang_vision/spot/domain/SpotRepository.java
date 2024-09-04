4ebb53efbde8550f5488c1854e08a85637dab937
king_0417 <73704053+gywns0417@users.noreply.github.com>
2024-09-04 18:13:49 +0900

    feat: Spot, SpotTranslation 리포지토리 작성

---
+++ src/main/java/com/gamgyul_code/halmang_vision/spot/domain/SpotRepository.java
@@ -0,0 +1,6 @@
+package com.gamgyul_code.halmang_vision.spot.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SpotRepository extends JpaRepository<Spot, Long> {
+}
---
+++ src/main/java/com/gamgyul_code/halmang_vision/spot/domain/SpotTranslationRepository.java
@@ -0,0 +1,7 @@
+package com.gamgyul_code.halmang_vision.spot.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SpotTranslationRepository extends JpaRepository<SpotTranslation, Long> {
+    SpotTranslation findByName(String name);
+}
