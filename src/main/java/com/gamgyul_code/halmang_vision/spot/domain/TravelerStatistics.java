3efd086fbdf627c962956a6dce6850f0c1c05cd3
king_0417 <73704053+gywns0417@users.noreply.github.com>
2024-09-04 18:13:33 +0900

    feat: Enum 클래스 예외 처리

--- src/main/java/com/gamgyul_code/halmang_vision/spot/domain/TravelerStatistics.java
+++ src/main/java/com/gamgyul_code/halmang_vision/spot/domain/TravelerStatistics.java
@@ -1,5 +1,23 @@
 package com.gamgyul_code.halmang_vision.spot.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum TravelerStatistics {
-    HIGH, MEDIUM, LOW
+    HIGH, MEDIUM, LOW;
+
+    @JsonCreator
+    public static TravelerStatistics from(String value) {
+        for (TravelerStatistics statistics : TravelerStatistics.values()) {
+            if (statistics.name().equals(value)) {
+                return statistics;
+            }
+        }
+        throw new IllegalStateException("Unexpected value: " + value); //TODO: 커스텀 에러 구현
+    }
+
+    @JsonValue
+    public String toValue() {
+        return this.name();
+    }
 }
