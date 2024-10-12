5b5dc0f6da4a12f280a4be3d344fbc3bb190699f
king_0417 <73704053+gywns0417@users.noreply.github.com>
2024-10-12 22:50:23 +0900

    feat: 설화별 관광지 목록 조회 JPA 메서드 작성

--- src/main/java/com/gamgyul_code/halmang_vision/spot/domain/SpotTranslationRepository.java
+++ src/main/java/com/gamgyul_code/halmang_vision/spot/domain/SpotTranslationRepository.java
@@ -1,9 +1,12 @@
 package com.gamgyul_code.halmang_vision.spot.domain;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SpotTranslationRepository extends JpaRepository<SpotTranslation, Long> {
     SpotTranslation findByName(String name);
 
     boolean existsBySpotIdAndLanguageCode(Long spotId, LanguageCode languageCode);
+
+    List<SpotTranslation> findAllBySpot_SpotCategoryAndLanguageCode(SpotCategory spotCategory, LanguageCode languageCode);
 }
