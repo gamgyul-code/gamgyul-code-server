38b6a9ff716c2bd734f5698cc2514d74ea1f6328
king_0417 <73704053+gywns0417@users.noreply.github.com>
2024-10-19 21:34:38 +0900

    feat: JPA 쿼리문 작성

--- src/main/java/com/gamgyul_code/halmang_vision/spot/domain/SpotTranslationRepository.java
+++ src/main/java/com/gamgyul_code/halmang_vision/spot/domain/SpotTranslationRepository.java
@@ -3,6 +3,8 @@ package com.gamgyul_code.halmang_vision.spot.domain;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SpotTranslationRepository extends JpaRepository<SpotTranslation, Long> {
     SpotTranslation findByName(String name);
@@ -11,7 +13,13 @@ public interface SpotTranslationRepository extends JpaRepository<SpotTranslation
 
     boolean existsBySpotIdAndLanguageCode(Long spotId, LanguageCode languageCode);
 
-    List<SpotTranslation> findAllBySpot_SpotCategoryAndLanguageCode(SpotCategory spotCategory, LanguageCode languageCode);
+    @Query("SELECT st FROM SpotTranslation st " +
+            "JOIN st.spot s " +
+            "WHERE :spotCategory IN elements(s.spotCategory) " +
+            "AND st.languageCode = :languageCode")
+    List<SpotTranslation> findAllBySpotCategoryAndLanguageCode(
+            @Param("spotCategory") SpotCategory spotCategory,
+            @Param("languageCode") LanguageCode languageCode);
 
     List<SpotTranslation> findAllBySpot_SpotRegionAndLanguageCode(SpotRegion spotRegion, LanguageCode languageCode);
 }
