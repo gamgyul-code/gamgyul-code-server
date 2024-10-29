9848afbd4667e10380f72208723495ff543de424
king_0417 <73704053+gywns0417@users.noreply.github.com>
2024-10-29 19:23:07 +0900

    feat: 내 경로 상세 조회 JPA 메서드 구현

--- src/main/java/com/gamgyul_code/halmang_vision/route/domain/RouteRepository.java
+++ src/main/java/com/gamgyul_code/halmang_vision/route/domain/RouteRepository.java
@@ -2,6 +2,7 @@ package com.gamgyul_code.halmang_vision.route.domain;
 
 import com.gamgyul_code.halmang_vision.member.domain.Member;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RouteRepository extends JpaRepository<Route, Long> {
@@ -9,4 +10,6 @@ public interface RouteRepository extends JpaRepository<Route, Long> {
     boolean existsByRouteNameAndMember(String routeName, Member member);
 
     List<Route> findAllByMember(Member member);
+
+    Optional<Route> findByIdAndMember(Long id, Member member);
 }
