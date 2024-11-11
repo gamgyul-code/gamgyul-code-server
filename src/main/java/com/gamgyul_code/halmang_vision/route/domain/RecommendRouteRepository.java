b3c36e4802b8f2a9acec36650a08a423bf25db0e
king_0417 <73704053+gywns0417@users.noreply.github.com>
2024-11-11 18:32:30 +0900

    feat: 추천 루트 JPA 리포지토리 생성

---
+++ src/main/java/com/gamgyul_code/halmang_vision/route/domain/RecommendRouteRepository.java
@@ -0,0 +1,6 @@
+package com.gamgyul_code.halmang_vision.route.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecommendRouteRepository extends JpaRepository<RecommendRoute, Long> {
+}
