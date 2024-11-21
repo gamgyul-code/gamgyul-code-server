c17abfb78b57984c5c3dbe8b65da5753a13f5fc4
king_0417 <73704053+gywns0417@users.noreply.github.com>
2024-11-21 20:33:31 +0900

    feat: 추천 경로 북마크 생성/삭제/조회 API 구현

--- src/main/java/com/gamgyul_code/halmang_vision/bookmark/domain/BookmarkGenerator.java
+++ src/main/java/com/gamgyul_code/halmang_vision/bookmark/domain/BookmarkGenerator.java
@@ -1,6 +1,7 @@
 package com.gamgyul_code.halmang_vision.bookmark.domain;
 
 import com.gamgyul_code.halmang_vision.member.domain.Member;
+import com.gamgyul_code.halmang_vision.route.domain.RecommendRoute;
 import com.gamgyul_code.halmang_vision.spot.domain.Spot;
 import com.gamgyul_code.halmang_vision.spot.domain.SpotCategory;
 import org.springframework.stereotype.Component;
@@ -8,11 +9,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class BookmarkGenerator {
 
-    public Bookmark generate(Spot spot, Member member, SpotCategory spotCategory) {
-        return Bookmark.builder()
+    public BookmarkSpot generate(Spot spot, Member member, SpotCategory spotCategory) {
+        return BookmarkSpot.builder()
                 .member(member)
                 .spot(spot)
                 .spotCategory(spotCategory)
+                .build();
+    }
+
+    public BookmarkRoute generate(RecommendRoute recommendRoute, Member member) {
+        return BookmarkRoute.builder()
+                .member(member)
+                .recommendRoute(recommendRoute)
                 .build();
     }
 }
--- src/main/java/com/gamgyul_code/halmang_vision/bookmark/presentation/BookmarkController.java
+++ src/main/java/com/gamgyul_code/halmang_vision/bookmark/presentation/BookmarkController.java
@@ -3,12 +3,16 @@ package com.gamgyul_code.halmang_vision.bookmark.presentation;
 import com.gamgyul_code.halmang_vision.bookmark.application.BookmarkService;
 import com.gamgyul_code.halmang_vision.global.utils.AuthPrincipal;
 import com.gamgyul_code.halmang_vision.member.dto.ApiMember;
+import com.gamgyul_code.halmang_vision.route.application.RouteService;
+import com.gamgyul_code.halmang_vision.route.dto.RouteDto.RouteResponse;
 import com.gamgyul_code.halmang_vision.spot.domain.SpotCategory;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookmarkController {
 
     private final BookmarkService bookmarkService;
+    private final RouteService routeService;
 
     @Operation(summary = "관광지 북마크 생성", description = "해당 회원에게 관광지 북마크를 생성한다.")
     @PostMapping("/spots/{spotId}/{spotCategory}")
@@ -33,5 +38,25 @@ public class BookmarkController {
     @DeleteMapping("/spots/{spotId}")
     public void deleteSpotBookmark(@PathVariable Long spotId, @Parameter(hidden = true) @AuthPrincipal ApiMember apiMember) {
         bookmarkService.deleteSpotBookmark(spotId, apiMember);
+    }
+
+    @Operation(summary = "추천 경로 북마크 생성", description = "해당 회원에게 추천 경로 북마크를 생성한다.")
+    @PostMapping("/recommend-routes/{recommendRouteId}")
+    public void createRecommendRouteBookmark(@PathVariable Long recommendRouteId,
+                                            @Parameter(hidden = true) @AuthPrincipal ApiMember apiMember) {
+        bookmarkService.createRecommendRouteBookmark(recommendRouteId, apiMember);
+    }
+
+    @Operation(summary = "추천 경로 북마크 삭제", description = "해당 회원에게 추천 경로 북마크를 삭제한다.")
+    @DeleteMapping("/recommend-routes/{recommendRouteId}")
+    public void deleteRecommendRouteBookmark(@PathVariable Long recommendRouteId,
+                                            @Parameter(hidden = true) @AuthPrincipal ApiMember apiMember) {
+        bookmarkService.deleteRecommendRouteBookmark(recommendRouteId, apiMember);
+    }
+
+    @GetMapping("/recommend-routes")
+    @Operation(summary = "추천 경로 북마크 조회", description = "사용자의 추천 경로 북마크를 조회한다.")
+    public List<RouteResponse> findAllRecommendRouteBookmarks(@Parameter(hidden = true) @AuthPrincipal ApiMember apiMember) {
+        return routeService.findAllMyRecommendRoutes(apiMember);
     }
 }
